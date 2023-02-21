277d63d6945d186587619158034db0725616107a
Etekuwoma Engineer Kelvin <87496337+Likel69@users.noreply.github.com>
2023-02-21 20:15:50 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-
 #include <stdio.h>
 
 /**
+ * This program prints _putchar
+ * main: houses all functions 
  * Return: 0 if succesful
  */
 int main(void)
 {
-printf("%s", "_putchar");
+ printf("%s", "_putchar");
 
-return (0);
+ return (0);
 }
