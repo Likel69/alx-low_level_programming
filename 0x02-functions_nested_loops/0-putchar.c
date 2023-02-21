0bba1b48a8c0ac70a8a80a7f3e0f8723f4ad0693
Etekuwoma Engineer Kelvin <87496337+Likel69@users.noreply.github.com>
2023-02-21 20:23:16 +0100

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * Description: program prints _putchar
- * main: houses all functions 
+ * Description: Prints _putchar followed by new line
+ * Main: houses all functions 
  * Return: 0 if succesful
- */
+*/
 int main(void)
 {
 printf("%s", "_putchar");
