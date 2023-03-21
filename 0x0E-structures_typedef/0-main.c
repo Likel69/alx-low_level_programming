2c4326e924076c688b6b68cf24d4d973ff2a1a42
Kelvin <kelvinlikel@gmail.com>
2023-03-21 07:04:58 +0000

    new

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	struct dog my_dog;
 
-	my_dog.name = "Candy";
-	my_dog.age = 4.8;
-	my_dog.owner = "Meco";
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	return (0);
 }
