1d9ecf406fa10e2ab4a8f00346628eac475bede8
Kelvin <kelvinlikel@gmail.com>
2023-05-04 06:27:09 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
+ * Description: print size of variables
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int a;
@@ -18,5 +21,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
+
