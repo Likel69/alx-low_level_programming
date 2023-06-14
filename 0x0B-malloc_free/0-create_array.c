77ec7f89d61ab0a708d9f7765c3c026c4ad1b7ab
Kelvin <kelvinlikel@gmail.com>
2023-06-14 19:05:55 +0000

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
